9baf5b67be53a49219d66556b25833281b5fd2a1
unknown <smkreter@gmail.com>
2020-07-08 20:13:16 -0500

    did the first part of the studio exercise; current issue is that the shuttle height number is not actually a number, but instead a string. haven't started second bit yet.

--- scripts.js
+++ scripts.js
@@ -1,2 +1,23 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+let takeoffButton = null;
+let flightStatus = null;
+window.addEventListener("load", function() {
+    takeoffButton = document.getElementById("takeoff");
+    flightStatus = document.getElementById("flightStatus");
+    let shuttleFlightScreen = document.getElementById("shuttleBackground");
+    let shuttleHeight = document.getElementById("spaceShuttleHeight");
+    takeoffButton.onclick = function() {
+        let takeOffConfirmation = window.confirm("Confirm that the shuttle is ready for takeoff")
+        if (takeOffConfirmation === true) {
+        flightStatus.innerHTML = "Shuttle in flight."
+        shuttleFlightScreen.style.background = "blue";
+        Number(shuttleHeight.innerHTML);
+        shuttleHeight.innerHTML = 10000;            
+        }
+    }
+    /*
+    takeoffButton.addEventListener("click", function(event) {
+        flightStatus.innerHTML = "Shuttle in flight.";
+        */
+    });
