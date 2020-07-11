956b221c63308293432db046e8922304d9dedc8f
unknown <smkreter@gmail.com>
2020-07-11 13:46:45 -0500

    I tried adding a span with the id of the js variable I wanted, but that's not how any of this works lol

--- scripts.js
+++ scripts.js
@@ -2,20 +2,23 @@
 // Remember to pay attention to page loading!
 let takeoffButton = null;
 let flightStatus = null;
+let shuttleAlt = 0;
 window.addEventListener("load", function() {
     takeoffButton = document.getElementById("takeoff");
     flightStatus = document.getElementById("flightStatus");
     let shuttleFlightScreen = document.getElementById("shuttleBackground");
-    let shuttleAlt = document.getElementById("spaceShuttleHeight");
+    let shuttleAlt = document.getElementById("shuttleAlt");
     //let shuttleHeight = shuttleHeightGraph.innerHTML;
     takeoffButton.onclick = function() {
         let takeOffConfirmation = window.confirm("Confirm that the shuttle is ready for takeoff")
         if (takeOffConfirmation === true) {
         flightStatus.innerHTML = "Shuttle in flight."
         shuttleFlightScreen.style.background = "blue";
+        shuttleAlt += 10000;
+        /*
         shuttleAlt.innerHTML = Number(shuttleAlt.innerHTML) + 10000;
         shuttleAlt.innerHTML = shuttleAlt.innerHTML.toString;
-        /*
+    
         shuttleHeight = Number(shuttleHeight);
         shuttleHeight = shuttleHeight + 10000;
        
