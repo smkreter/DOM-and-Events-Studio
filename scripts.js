c7cdadbf0cf44df7458c1eb38e5533e8ab0de019
unknown <smkreter@gmail.com>
2020-07-10 23:53:53 -0500

    now there's an error in the innerHTML of the shuttleAlt so that's progress. I think I don't know what kind of data that is and that's the source of my problem. also I am tired.

--- scripts.js
+++ scripts.js
@@ -6,14 +6,22 @@ window.addEventListener("load", function() {
     takeoffButton = document.getElementById("takeoff");
     flightStatus = document.getElementById("flightStatus");
     let shuttleFlightScreen = document.getElementById("shuttleBackground");
-    let shuttleHeight = document.getElementById("spaceShuttleHeight");
+    let shuttleAlt = document.getElementById("spaceShuttleHeight");
+    //let shuttleHeight = shuttleHeightGraph.innerHTML;
     takeoffButton.onclick = function() {
         let takeOffConfirmation = window.confirm("Confirm that the shuttle is ready for takeoff")
         if (takeOffConfirmation === true) {
         flightStatus.innerHTML = "Shuttle in flight."
         shuttleFlightScreen.style.background = "blue";
-        Number(shuttleHeight.innerHTML);
-        shuttleHeight.innerHTML = 10000;            
+        shuttleAlt.innerHTML = Number(shuttleAlt.innerHTML) + 10000;
+        shuttleAlt.innerHTML = shuttleAlt.innerHTML.toString;
+        /*
+        shuttleHeight = Number(shuttleHeight);
+        shuttleHeight = shuttleHeight + 10000;
+       
+        shuttleHeight.innerHTML = Number(shuttleHeight.innerHTML);
+        shuttleHeight.innerHTML += 10000;  
+        */          
         }
     }
     /*
